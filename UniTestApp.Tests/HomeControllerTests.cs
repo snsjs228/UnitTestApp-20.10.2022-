8fe555155bb1c09f3ad6191cdc1b82da64ffcfca
user <user@305-03>
2022-10-20 12:46:19 +0300

    Добавьте файлы проекта.

---
+++ UniTestApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniTestApp.Controllers;
+using Xunit;
+
+namespace UniTestApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexViewDataMessage()
+        {
+            //Arrange
+            HomeControllerTests controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.Equal("Hello world!", result?.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void IndexViewResultNotNull()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void IndexViewNameEqualIndex()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.Equal("Index", result ? ViewName);
+        }
+    }
+}
